3a505861023986b79edf6042f2c3aa3993385c15
yangxin45 <yangxin45@wanda.cn>
2017-06-08 11:45:21 +0800

    bugfix: prometheus

--- kubernetes/prometheus/deploy.sh
+++ kubernetes/prometheus/deploy.sh
@@ -3,35 +3,6 @@
 script_root=`dirname "${BASH_SOURCE}"`
 source $script_root/../env.sh
 
-MAX_TASK_WAIT_RETRIES=${MAX_TASK_WAIT_RETRIES:-300}
-
-function update_spinner_value () {
-  spinner='-\|/'
-  cur_spinner=${spinner:$(($1%${#spinner})):1}
-}
-
-function wait_for_complete () {
-  url=$1
-  response=0
-  counter=0
-
-  while [ $counter -lt $MAX_TASK_WAIT_RETRIES ]; do
-    response=$(curl --write-out %{http_code} --silent --output /dev/null $url)
-    echo -en "\r$url: waiting for return http_code:200..."
-    if [ $response -eq 200 ]
-    then
-      echo Complete
-      return 0
-    fi
-    update_spinner_value $counter
-    echo -n $cur_spinner
-    let counter=counter+1
-    sleep 1
-  done
-  echo Timed out
-  return -1
-}
-
 c=create
 if [ "$1" == "d" ]; then
 	c=delete
