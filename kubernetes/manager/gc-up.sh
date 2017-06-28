df99b402d810b9b09730f7396dbdcd1a6799c82e
yangxin45 <yangxin45@wanda.cn>
2017-06-28 16:36:07 +0800

    bugfix: tidb gc yaml

--- kubernetes/manager/gc-up.sh
+++ kubernetes/manager/gc-up.sh
@@ -6,15 +6,10 @@ script_root=`dirname "${BASH_SOURCE}"`
 source $script_root/../env.sh
 
 version=latest
-DATA_VOLUME=${DATA_VOLUME:-''}
-tidbdata_volume='emptyDir: {}'
-if [ -n "$DATA_VOLUME" ]; then
-  tidbdata_volume="hostPath: {path: ${DATA_VOLUME}}"
-fi
 
  echo "Creating tidb gc daemonset..."
 sed_script=""
-for var in version tidbdata_volume; do
+for var in version; do
   sed_script+="s,{{$var}},${!var},g;"
 done
 cat gc-daemonset.yaml | sed -e "$sed_script" | $KUBECTL $KUBECTL_OPTIONS create -f -
