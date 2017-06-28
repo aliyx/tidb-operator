b10a3f21a62d8ca61b2b7b114be62cd164d08e4b
yangxin45 <yangxin45@wanda.cn>
2017-06-28 17:04:18 +0800

    add permissions to tidb gc

--- kubernetes/manager/gc-down.sh
+++ kubernetes/manager/gc-down.sh
@@ -7,3 +7,5 @@ source $script_root/../env.sh
 
 echo "Deleting tidb gc daemonset..."
 $KUBECTL $KUBECTL_OPTIONS delete daemonset tidb-gc
+$KUBECTL $KUBECTL_OPTIONS delete ServiceAccount tidb-gc
+$KUBECTL $KUBECTL_OPTIONS delete ClusterRoleBinding tidb-gc
--- kubernetes/manager/gc-up.sh
+++ kubernetes/manager/gc-up.sh
@@ -5,11 +5,12 @@ set -e
 script_root=`dirname "${BASH_SOURCE}"`
 source $script_root/../env.sh
 
+namespace=$NS
 version=latest
 
  echo "Creating tidb gc daemonset..."
 sed_script=""
-for var in version; do
+for var in namespace version; do
   sed_script+="s,{{$var}},${!var},g;"
 done
 cat gc-daemonset.yaml | sed -e "$sed_script" | $KUBECTL $KUBECTL_OPTIONS create -f -
