36ddac8aa575a2344cbbbd143459796d02f38191
yangxin45 <yangxin45@wanda.cn>
2017-07-06 14:56:47 +0800

    Reduce tikv image size

--- docker/tikv/build.sh
+++ docker/tikv/build.sh
@@ -3,6 +3,8 @@
 script_root=`dirname "${BASH_SOURCE}"`
 source $script_root/../../dev.env
 
+rm -rf base
+
 set -e
 
 VERSION=${VERSION:-'latest'}
@@ -12,4 +14,14 @@ echo "*Starting build tikv image..."
 echo "*  Proxy: $DPROXY"
 echo "*  Image: $REGISTRY/ffan/rds/tikv:$VERSION"
 echo "****************************" 
-docker build $DPROXY -t $REGISTRY/ffan/rds/tikv:$VERSION ./
+(docker build $DPROXY -t $REGISTRY/ffan/rds/tikv:$VERSION-base -f dockerfile ./)
+
+# Extract files from ffan/rds/pd image
+mkdir base
+docker run -ti --rm -v $PWD/base:/base -u $UID --entrypoint sh $REGISTRY/ffan/rds/tikv:$VERSION-base -c 'cp -f /tikv-server /base/tikv-server'
+
+# Build ffan/rds/tikv image
+docker build -t $REGISTRY/ffan/rds/tikv:$VERSION -f dockerfile_lite ./
+
+# Clean up temporary files
+rm -rf base
